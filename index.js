3142ccc932e55c4cc8c55dd265d94e56a3d6f1be
gururajbn <n.gururajbn@gmail.com>
2016-03-25 00:18:30 +0530

    second commit

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-var expressio= require('express.io');
-var app= expressio();
+var express= require('express');
+var swig= require('swig');
+var io= require('socket.io');
+
+var app= express();
 var port= process.env.PORT | 8000;
 
-
+app.use('/','./routes/index.js');
 
 app.listen(port,function(err){
     console.log("listing on "+port);
