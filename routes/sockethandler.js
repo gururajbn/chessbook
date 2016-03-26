df2ae2601e5ab1845e7ca5dfdae330bb969627d6
gururajbn <n.gururajbn@gmail.com>
2016-03-26 18:01:57 +0530

    added socketHandler client/server

---
+++ routes/sockethandler.js
@@ -0,0 +1,18 @@
+
+function socketRouter(io){
+    io.sockets.on('connection',function(socket){
+        console.log(socket.id+' connected');
+
+        socket.on('join',function(data){
+            socket.join(data.gameId);
+            console.log(socket.rooms);
+        });
+
+        socket.on('changeInPosition',function(data){
+            console.log("updatePostition from "+socket.id+"\n "+data);
+            socket.broadcast.to(data.gameId).emit('updatePostition',data);
+        })
+    })
+}
+
+exports.socketRouter= socketRouter;
